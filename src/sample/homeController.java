579889c43b65d09aef2e263572938f3e65a4c0c9
chunqi815 <chunqi815@gmail.com>
2021-12-07 18:28:09 +0800

    Exit Button Usable on Menu

--- src/sample/homeController.java
+++ src/sample/homeController.java
@@ -1,24 +1,22 @@
 package sample;
 
 
-import javafx.event.ActionEvent;
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.fxml.Initializable;
-import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-import java.net.URL;
-import java.util.ResourceBundle;
+
 
 public class homeController {
 
     @FXML
     private Button startButton;
+
 
     @FXML
     private void onStartButtonClick() throws IOException {
@@ -26,5 +24,12 @@ public class homeController {
         Scene scene = new Scene(root,600,450);
         Stage stage = (Stage)startButton.getScene().getWindow();
         stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    private void onExitButtonClick(){
+        Platform.exit();
+        System.exit(0);
     }
 }
