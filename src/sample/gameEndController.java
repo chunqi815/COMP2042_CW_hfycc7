6b4c72a274167941c706713d551c4daff6ea3839
chunqi815 <chunqi815@gmail.com>
2021-12-07 18:36:43 +0800

    Added Main Menu Button

--- src/sample/gameEndController.java
+++ src/sample/gameEndController.java
@@ -6,22 +6,21 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
-
-import java.awt.*;
 import java.io.IOException;
 
 public class gameEndController {
 
     @FXML
-    private AnchorPane gameEndScene;
-
+    private void onRestartButtonClick(ActionEvent actionEvent) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("gamepage.fxml"));
+        Scene scene = new Scene(root,600,450);
+        Stage stage = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
+    }
     @FXML
-    private Button restartButton;
-
-    @FXML
-    private void onGameEndButtonClick(ActionEvent actionEvent) throws IOException {
+    private void onMenuButtonClick(ActionEvent actionEvent) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("homepage.fxml"));
         Scene scene = new Scene(root,450,300);
         Stage stage = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
